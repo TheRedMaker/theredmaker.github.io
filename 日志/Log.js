79a681cf23ce1528e2196a954933cd1422a99f98
myworldzycpc <myworldzhangyichen@qq.com>
2022-02-07 21:30:24 +0800

    日志-myworldzycpc

--- 日志/Log.js
+++ 日志/Log.js
@@ -41,4 +41,5 @@ export function registerLogs() {
     registerLog(new Log("2022年1月25日", "得到了粘液球", engine$bs3, "./2022年1月25日/index.html"));
     registerLog(new Log("2022年1月28日", "金制背包，钻石", engine$bs5, "./2022年1月28日/index.html"));
     registerLog(new Log("2022年2月3日", "不祥之兆真不详", engine$bs5, "./2022年2月3日/index.html"));
+    registerLog(new Log("2022年2月4日", "又一个不祥之兆", engine$bs5, "./2022年2月4日/index.html"));
 }
